a9eb655fab57ab69b51f55c9bd5a44e1e1763116
seidhaf <34340707+seidhaf@users.noreply.github.com>
2017-12-27 22:10:56 +0100

    Update FormUpload.jsx

--- src/components/FormUpload.jsx
+++ src/components/FormUpload.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import {Link} from 'react-router-dom';
 import {Form, FormGroup, formTitle, Col, FormControl, ControlLabel, Label, Button, Glyphicon} from 'react-bootstrap';
-import './FormUpload.css';
+
 
 class FormUpload extends React.Component{
   render() {
