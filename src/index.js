38f40e26183bebb398151f703cfeadf756eea139
seidhaf <34340707+seidhaf@users.noreply.github.com>
2018-02-21 20:16:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers } from 'redux';
+import {Provider} from 'react-redux';
 import { reducer as formReducer } from 'redux-form';
 import { routeReducers } from 'react-router-redux';
+import { Values } from "redux-form-website-template";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store, { history } from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+  <div style={{ padding: 15 }}>
+    <App />
+    <div class ="container">
+    <Values form="content" />
+    </div>
+  </div>
+</Provider>, document.getElementById('root')
+);
 registerServiceWorker();
