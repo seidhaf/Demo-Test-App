fe627d19c9427cd629bcf289504ba786cf618622
seidhaf <34340707+seidhaf@users.noreply.github.com>
2018-03-16 22:53:18 +0100

    Update Link.jsx

--- src/pages/Link.jsx
+++ src/pages/Link.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import {Row, Col} from 'react-bootstrap';
-import FormLink from '../components/FormLink.jsx';
+import LinkForm from '../components/LinkForm.jsx';
 
 class Link extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <FormLink />
+        <LinkForm />
       </div>
     );
   }
